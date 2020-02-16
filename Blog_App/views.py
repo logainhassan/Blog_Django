32c9f65435335d60f646910faf9a05a21626baf4
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-02-17 01:58:41 +0200

    clear urls

--- Blog_App/views.py
+++ Blog_App/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
 
-from django.http import HttpRequest, HttpResponse
-from django.http import HttpResponseRedirect
-from .models import Forbidden
 # Create your views here.
-
-
-def home(request):
-    return render(request , 'Blog_App/index.html')
-
