c3548a81141a4facf5e9e1cb95254e682e369faa
Mohamed Gaber <gaberm514@gmail.com>
2020-02-18 02:30:03 +0200

    add all category view

--- Blog_App/views.py
+++ Blog_App/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
@@ -8,3 +9,13 @@ def table(request):
 
 def user(request):
 	return render(request, 'admin/user.html')
+
+def all_Category(request):
+	objects=Category.objects.all()
+	fields=Category.get_model_fields(Category)
+	context={
+		'Categories' : objects ,
+		 'fields' : fields ,
+		 'title' : "Categories"
+		 }
+	return render(request,'admin/tables.html',context)
