46375408c038be63224ea676cb31e3f241193340
logain <logainhassan@github.com>
2020-02-18 18:41:50 +0200

    make user form and users list

--- Blog_App/views.py
+++ Blog_App/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+from Blog_App.forms import UserForm
+from Blog_App.models import Users,Posts
 # Create your views here.
 
 
 def table(request):
-	return render(request, 'admin/tables.html')
+	all_users = Users.objects.all()
+	context ={'all_users' : all_users}
+	return render(request, 'admin/tables.html',context)
 
 def user(request):
-	return render(request, 'admin/user.html')
+	form = UserForm()
+	return render(request, 'admin/user.html',{'form':form})
