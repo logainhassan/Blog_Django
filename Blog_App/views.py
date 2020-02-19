6489510dd852df11c5558a0198b6a1c34e5e2d1b
Mohamed Gaber <gaberm514@gmail.com>
2020-02-19 19:37:07 +0200

    add edit category view

--- Blog_App/views.py
+++ Blog_App/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-from Blog_App.forms import UserForm
+from Blog_App.forms import UserForm , Category_form
 from Blog_App.models import Users,Posts
 from django.http import HttpResponseRedirect
 
@@ -58,3 +58,21 @@ def all_Category(request):
 		 'title' : "Categories"
 		 }
 	return render(request,'admin/Cat_table.html',context)
+
+
+
+def edit_Category(request,num):
+	cat_obj=Category.objects.filter(pk=num).first()
+	if(request.method=="POST"):
+		cat_form=Category_form(request.POST,instance=cat_obj)
+		if cat_form.is_valid():
+			cat_form.save()
+		return HttpResponseRedirect	("/Blog_App/category")
+	else:
+		cat_form=Category_form(instance=cat_obj)
+		context={
+			'cat_form':cat_form,
+			'title':'Edit'
+			}
+		
+		return render(request,'admin/category.html',context)
