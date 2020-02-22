15594806d7aa1a04f8e14072cc0bb05c5b660111
MohamedGaber <gaberm514@gmail.com>
2020-02-23 00:52:22 +0200

    add postDetails and all posts views

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from Admin.models import *
+from django.shortcuts import get_object_or_404
+from Blog.forms import *
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def allPosts(request) :
+    return render(request,'Blog/allPosts.html',)   
+
+def PostDetails(request,num):
+    post=get_object_or_404(Post,id=num)
+    comments=Comment.objects.filter(post=post,reply=None).order_by('id')
+
+
+    if request.method=='POST':
+        comment_form=CommentForm(request.POST or None)
+        if comment_form.is_valid():
+            content=request.POST.get('content')
+            reply_id=request.POST.get('comment_id')  
+            replys_qs=None
+            # if reply_id:
+            #     replays_qs=Comment.objects.get(id=reply_id)
+            #     print(replays_qs)
+            comment=Comment.objects.create(post=post,content=content,user_id=2,reply_id=reply_id)
+            comment.save()
+            return HttpResponseRedirect(post.get_absolute_url())
+            # comment_form.save()
+    else:
+        comment_form= CommentForm()        
+
+    
+    context={
+        'post':post,
+        'comments':comments,
+        'commentForm':comment_form
+    } 
+    return render(request,'Blog/postDetails.html',context)  
