0e692946f582585d2d3b194451b12ef64c1406ef
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2020-02-23 07:28:28 +0200

    edit user_name

--- Admin/models.py
+++ Admin/models.py
@@ -37,7 +37,7 @@ class Post(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
-        return '{}{}'.format(self.title,str(self.user.name))
+        return '{}{}'.format(self.title,str(self.user.user_name))
     def get_absolute_url(self):
         return "/post/%i" % self.pk
 
