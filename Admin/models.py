734de1ae3226057bc55d0f10a7b5ecb5af2c88f5
MohamedGaber <gaberm514@gmail.com>
2020-02-26 18:10:29 +0200

    lower max length for  2 fields in myuser model , add subscribers field to category model

--- Admin/models.py
+++ Admin/models.py
@@ -28,10 +28,10 @@ class MyUserManager(BaseUserManager):
 
 
 class MyUser(AbstractBaseUser):
-  username = models.CharField(max_length=200, unique=True,verbose_name='username')
+  username = models.CharField(max_length=150, unique=True,verbose_name='username')
   first_name =  models.CharField(blank=True, max_length=30, verbose_name='first name')
   last_name = models.CharField(blank=True, max_length=150, verbose_name='last name')
-  email = models.EmailField(max_length=254, unique=True,verbose_name='email address')
+  email = models.EmailField(max_length=150, unique=True,verbose_name='email address')
   password = models.CharField(max_length=100,validators=[RegexValidator(regex=PASSWORD_REGEX,
         message="Password must contain at least one letter, at least one number, and be longer than eight charaters."
         ,code="invalid_password")],)
@@ -74,6 +74,7 @@ class Forbidden(models.Model):
 
 class Category(models.Model):
     Name = models.CharField(max_length=100)
+    subscribes=models.ManyToManyField(MyUser,related_name="users")
     def get_model_fields(self):
         return self._meta.fields
     def __str__(self):
@@ -95,7 +96,7 @@ class Category(models.Model):
 #     role = models.IntegerField(default=2, choices=ROLES)
 #     image = models.ImageField(upload_to='Users/',max_length=500,default=None)
 
-
+    
 class Tag(models.Model):
     name=models.CharField(max_length=100)
     def get_model_fields(self):
@@ -115,7 +116,7 @@ class Post(models.Model):
     category=models.ManyToManyField(Category,related_name='posts')
 
     def __str__(self):
-        return '{}{}'.format(self.title,str(self.user.user_name))
+        return '{}{}'.format(self.title,str(self.user.username))
     def get_absolute_url(self):
         return "/post/%i" % self.pk
 
